add0760c195024ddf949af204942d21f41c481c2
Diego Abelenda <diego.abelenda@camptocamp.com>
2018-04-17 15:26:50 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM debian:stretch
+
+EXPOSE 9000
+
+ENV RELEASE=stretch \
+    LANGUAGE=en_US.UTF-8 \
+    LC_ALL=en_US.UTF-8 \
+    LANG=en_US.UTF-8 \
+    WEBHOOK_VERSION=2.6.8
+
+RUN apt-get update \
+  && apt-get install -y make python3 python3-requests python3-yaml \
+  && apt-get clean \
+  && rm -rf /var/lib/apt/lists/*
+
+COPY snow-hook.json /etc/webhook/snow-hook.json
+COPY snow-hook.py /snow-hook.py
+
+# Install webhook
+RUN apt-get update \
+    && apt-get install -y wget \
+    && wget https://github.com/adnanh/webhook/releases/download/${WEBHOOK_VERSION}/webhook-linux-amd64.tar.gz \
+    && tar xzf webhook-linux-amd64.tar.gz \
+    && mv webhook-linux-amd64/webhook /usr/local/bin/webhook \
+    && rm -f webhook-linux-amd64.tar.gz \
+    && apt-get remove -y --purge wget \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+COPY docker-entrypoint.sh /docker-entrypoint.sh
+COPY /docker-entrypoint.d/* /docker-entrypoint.d/
+
+ENTRYPOINT ["/docker-entrypoint.sh"]
---
+++ snow-hook.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import requests
+import yaml
+import json
+import os
+import sys
+
+URL = os.environ['SNOW_API_URL']
+
+PASSWORD_FILE = os.environ['SNOW_API_PASSWORD_FILE']
+
+HEADERS = {"Content-Type":"application/json","Accept":"application/json"}
+
+def getPass(filename, service):
+    username = None
+    password = None
+    with open(filename) as f:
+        passdata = yaml.load(f)
+        if service in passdata:
+            username = passdata[service].get('username', None)
+            password = passdata[service].get('password', None)
+    if username == None or password == None:
+        return None
+    return ( username, password )
+
+alertmanagerdata = json.loads(sys.argv[1])
+
+data = {
+        "u_business_service" : alertmanagerdata['groupLabels']['project'],
+        "u_priority" : 5 if alertmanagerdata['status'] == "resolved" else 1,
+        "u_short_description" : 'Automatic Service Down via Camptocamp Monitoring.',
+        "u_description" : 'ToDo',
+}
+auth = getPass(PASSWORD_FILE, data['u_business_service'])
+if auth == None:
+    print('no entry in password file for service "' + data['u_business_service'] + '"')
+    sys.exit(1)
+
+response = requests.post(URL, auth=auth, headers=HEADERS, data=json.dumps(data).strip())
+
+print("ServiceNow returned %s" % response.status_code)
