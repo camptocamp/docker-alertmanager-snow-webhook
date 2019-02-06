c7b8f9a268e80e43ca825647e05e3936ac7ca467
Christophe Burki <christophe.burki@camptocamp.com>
2019-02-06 17:18:49 +0100

    Distinguish the up and description and also set it to the short description

--- snow-hook.py
+++ snow-hook.py
@@ -27,11 +27,17 @@ def getPass(filename, service):
 
 alertmanagerdata = json.loads(sys.argv[1])
 
+priority = 1
+description = 'Automatic Service Down via Camptocamp Monitoring.'
+if alertmanagerdata['status'] == 'resolved':
+    priority = 5
+    description = 'Automatic Service Up via Camptocamp Monitoring.'
+
 data = {
         "u_business_service" : alertmanagerdata['groupLabels'][LABEL_NAME],
-        "u_priority" : 5 if alertmanagerdata['status'] == "resolved" else 1,
-        "u_short_description" : 'Automatic Service Down via Camptocamp Monitoring.',
-        "u_description" : 'ToDo',
+        "u_priority" : priority,
+        "u_short_description" : description,
+        "u_description" : description,
 }
 auth = getPass(PASSWORD_FILE, data['u_business_service'])
 if auth == None:
